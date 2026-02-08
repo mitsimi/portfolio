9818ed9d4f56b7280c7b893b572c74989babf826
mitsimi <noah.fuereder@gmail.com>
2026-02-08 23:50:33 +0100

    remove location

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -12,11 +12,6 @@ export const Footer = ({ data, strings }: FooterProps) => {
   return (
     <footer className="relative px-6 md:px-12 py-12 bg-bg border-t-4 border-black/10">
       <div className="max-w-450 mx-auto">
-        <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-6 mb-6">
-          <BoldLabel className="text-black/50 flex gap-2">
-            <MapPin size={14} /> {data.location.toUpperCase()}
-          </BoldLabel>
-        </div>
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-4">
           <BoldLabel className="text-black/30 flex gap-2">
             <AtSign size={14} /> {new Date().getFullYear()}{" "}
